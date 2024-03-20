267e0d7297f3e7d0ed3acbbd45fadfc843de604d
crowlke <caiostarke@hotmail.com>
2024-03-20 15:33:26 -0300

    group test

--- src/components/greeting/Greeting.test.tsx
+++ src/components/greeting/Greeting.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
-test('Greet renders correctly', () => {
-    render(<Greeting />)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument();
-})
-
-test('Greet renders props if exists', () => {
-    render(<Greeting name="John" />)
-    const textElement = screen.getByText(/hello john/i)
-    expect(textElement).toBeInTheDocument();
+describe('greet', () => {
+    test('renders correctly', () => {
+        render(<Greeting />)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument();
+    })
+    
+    test('renders props if exists', () => {
+        render(<Greeting name="John" />)
+        const textElement = screen.getByText(/hello john/i)
+        expect(textElement).toBeInTheDocument();
+    })
 })
